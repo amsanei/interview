584fbc422092cfd56262cb0a057dbfc4f7b37908
Amir mohammad <am.sanei79@gmail.com>
2025-03-15 23:17:49 +0330

    Add barbers page loading

--- src/app/barbers/page.tsx
+++ src/app/barbers/page.tsx
@@ -26,19 +26,19 @@ export default async function page() {
           />
           <div className="mt-2">
             <div className="flex justify-between items-center mb-4">
-            <h2 className="font-bold text-lg">{item.fullname}</h2>
-            <div className="flex gap-1 text-sm">
+              <h2 className="font-bold text-lg">{item.fullname}</h2>
+              <div className="flex gap-1 text-sm">
                 <div>{item.reviews_count}</div>
                 <div>*</div>
-            </div>
-            </div>
-            <div className="flex gap-2">
-                <div>a:</div>
-                <div>{item.address}</div>
+              </div>
             </div>
             <div className="flex gap-2">
-                <div>p:</div>
-                <div>{item.phone_number}</div>
+              <div>a:</div>
+              <div>{item.address}</div>
+            </div>
+            <div className="flex gap-2">
+              <div>p:</div>
+              <div>{item.phone_number}</div>
             </div>
             <div className="flex flex-wrap gap-2 mt-4">
               {item.services.map((service: string) => (
